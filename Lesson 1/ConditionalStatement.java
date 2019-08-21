23767f1d2a9f874f50d700c07023a063d23695cc
Pilgrim04 <pilgrim04@gmail.com>
2019-08-21 15:23:34 +0300

    implement HW_1

---
+++ Lesson 1/ConditionalStatement.java
@@ -0,0 +1,34 @@
+public class ConditionalStatement{
+	public static void main(String[] args){
+		int yourAge = (int) (Math.random() * 100);
+		if (yourAge > 20) {
+			System.out.println("Your age is more than 20");
+		}
+		boolean youAreMale = true;
+		if (youAreMale) {
+			System.out.println("Male");
+		}
+		boolean youAreFemale = false;
+		if (youAreFemale) {
+			System.out.println("Female");
+		}
+		double yourHeight = Math.random() * 0.4 + 1.6;
+		if (yourHeight < 1.80) {
+			System.out.println("NotHigh");
+		} else {
+			System.out.println("High");
+		}
+		char yourNameFirstLetter = 'P';
+		boolean yourNameBeginsWithLetterM = (yourNameFirstLetter == 'M'); 
+		boolean yourNameBeginsWithLetterI = (yourNameFirstLetter == 'I');
+		if (yourNameBeginsWithLetterM) {
+			System.out.println("Your name begins with M");
+		} else {
+			if (yourNameBeginsWithLetterI) {
+				System.out.println("Your name begins with I");
+			} else {
+				System.out.println("Your name doesn't begin with letters M or I");
+			}
+		} 
+	}
+}
