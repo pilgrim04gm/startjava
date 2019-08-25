dc894c548576c0a612558b4bb51ede7193415ea3
Pilgrim04 <pilgrim04@gmail.com>
2019-08-25 14:50:05 +0300

    adding Person

---
+++ Lesson 2/Person.java
@@ -0,0 +1,28 @@
+public class Person {
+
+	char gender = 'm';
+	String name = "Pilgrim";
+	double height = 1.70;
+	double weight = 70.5;
+	int age = 30;
+
+	void go() {
+
+	}
+
+	void sit() {
+
+	}
+
+	void run() {
+
+	}
+
+	String speak() {
+		return("");
+	}
+
+	void studyJava() {
+
+	}
+}
