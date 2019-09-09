0efb8a93d7152cbe491215a9a1830596c243ae70
Pilgrim04 <pilgrim04@gmail.com>
2019-09-09 20:50:16 +0300

    fixing GuessNumber

--- Lesson 2/GuessNumber.java
+++ Lesson 2/GuessNumber.java
@@ -11,27 +11,31 @@ public class GuessNumber {
 	}
 
 	public void startGame() {
-		do {
-		} while (!compareNumbers(firstPlayer)&&!compareNumbers(secondPlayer));
+		if (!makeMove(firstPlayer) && !makeMove(secondPlayer)) {
+			startGame();
+		} 
+	}
+
+	public boolean makeMove(Player player) {
+		inputNumber(player);
+		return compareNumbers(player);
 	}
 
 	public boolean compareNumbers(Player player) {
-		System.out.print(player.getName() + ": ");
-		inputNumber(player);
 		if (player.getNumber() == numberToGuess) {
 			System.out.println("Computer: Game over! " + player.getName() + " is the winner!");
 			return true;
-		} else {
-			if (player.getNumber() > numberToGuess) {
-				System.out.println("Computer: < ");
-			} else {
+		} else if (player.getNumber() > numberToGuess) { 
+			System.out.println("Computer: < ");
+			} else { 
 				System.out.println("Computer: > ");
 			}
-			return false;
-		}
+		return false;
+			
 	}
 
 	public void inputNumber(Player player) {
+		System.out.print(player.getName() + ": ");
 		player.setNumber(sc.nextInt());
 	}
 }
